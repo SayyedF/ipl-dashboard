c1558c751b65a62cea9206f4dd5e79ef8a4d3e68
S F Jeelani <sayyedfkj@gmail.com>
2022-03-03 17:46:44 +0000

    Organised imports

--- src/main/java/com/jilani/ipldashboard/controller/TeamController.java
+++ src/main/java/com/jilani/ipldashboard/controller/TeamController.java
@@ -5,7 +5,6 @@ import com.jilani.ipldashboard.model.Team;
 import com.jilani.ipldashboard.repository.MatchRepository;
 import com.jilani.ipldashboard.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,18 +23,19 @@ public class TeamController {
     @GetMapping("/teams/{teamName}")
     public Team getTeam(@PathVariable String teamName) {
         Team team = teamRepository.getTeamByTeamName(teamName);
-        team.setLatestMatches(matchRepository.getLatestMatches(teamName,4));
+        team.setLatestMatches(matchRepository.getLatestMatches(teamName, 4));
         return team;
     }
 
     @GetMapping("/teams/{teamName}/matches")
-    public List<Match> getTeamMatchesByYear(@PathVariable String teamName, @RequestParam(defaultValue = "2020") Integer year) {
-        //return matchRepository.getTeamMatchesByYear(teamName, year);
-        return matchRepository.getMatchesByYear(teamName,year);
+    public List<Match> getTeamMatchesByYear(@PathVariable String teamName,
+            @RequestParam(defaultValue = "2020") Integer year) {
+        // return matchRepository.getTeamMatchesByYear(teamName, year);
+        return matchRepository.getMatchesByYear(teamName, year);
     }
 
     @GetMapping("/teams")
-    public List<String> getTeamNames(){
+    public List<String> getTeamNames() {
         return teamRepository
                 .findAll()
                 .stream()
